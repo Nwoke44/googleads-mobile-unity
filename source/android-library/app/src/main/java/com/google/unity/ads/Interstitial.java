77ff554696dd92a275de5358268de75332ea9a76
samuelstow <samuelstow@google.com>
2020-02-20 14:25:10 -0800

    Internal change.
    PiperOrigin-RevId: 296297012

--- source/android-library/app/src/main/java/com/google/unity/ads/Interstitial.java
+++ source/android-library/app/src/main/java/com/google/unity/ads/Interstitial.java
@@ -138,6 +138,7 @@ public class Interstitial {
                 });
             }
         });
+
     }
 
     /**
--- source/android-library/app/src/main/java/com/google/unity/ads/UnityRewardedAdCallback.java
+++ source/android-library/app/src/main/java/com/google/unity/ads/UnityRewardedAdCallback.java
@@ -4,7 +4,8 @@ package com.google.unity.ads;
  * An interface form of {@link UnityRewardedAdCallback} that can be implemented via
  * {@code AndroidJavaProxy} in Unity to receive ad events synchronously.
  */
-public interface UnityRewardedAdCallback {
+public interface UnityRewardedAdCallback
+{
     void onRewardedAdLoaded();
 
     void onRewardedAdFailedToLoad(String errorReason);
@@ -17,4 +18,3 @@ public interface UnityRewardedAdCallback {
 
     void onUserEarnedReward(String type, float amount);
 }
-
